c906742493bae173fe77f415c024221c75d328d2
Agustin Mellado <aguxmellado@gmail.com>
2022-10-08 12:40:57 -0300

    creacion de menu con listas de arreglo

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -6,13 +6,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-bar.component.css'],
 })
 export class SideBarComponent implements OnInit {
-  linksMenu: Array<any> = [
+  //menu principal
+  mainMenu: {
+    //declaracion de propiedades del tipo array.
+    defaultOptions: Array<any>;
+    accessLink: Array<any>;
+  } = {
+    //inicializacion
+    defaultOptions: [],
+    accessLink: [],
+  };
 
-    { name: 'Home', icon: 'uil-estate' },
-    { name: 'Buscar', icon: '' },
-  ];
+  customOptions: Array<any> = [];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    //indico los valores que iran dentro del arreglo.
+    this.mainMenu.defaultOptions = [
+      //valores de la listas de arreglos
+      {
+        name: 'Home',
+        icon: 'uil-estate',
+        router: ['/'],
+      },
+      {
+        name: 'Buscar',
+        icon: 'uil-search',
+        router: ['/', 'history'],
+      },
+      {
+        name: 'Tu Biblioteca',
+        icon: 'uil-chart',
+        router: ['/', 'favorites'],
+      },
+    ];
+    //---------------------------------------------
+    this.mainMenu.accessLink = [
+      {
+        name: 'Crear lista',
+        icon: 'uil-plus-square',
+      },
+      {
+        name: 'Canciones que te gustan',
+        icon: 'uil-heard-medical',
+      },
+    ];
+    //---------------------------------------------
+    this.customOptions = [
+      {
+        name: 'Mi lista º1',
+        router: ['/'],
+      },
+      {
+        name: 'Mi lista º2',
+        router: ['/'],
+      },
+      {
+        name: 'Mi lista º3',
+        router: ['/'],
+      },
+      {
+        name: 'Mi lista º4',
+        router: ['/'],
+      },
+    ];
+  }
 }
