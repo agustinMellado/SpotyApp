e609b218bd44aff37673ebce016291579c339846
Agustin Mellado <aguxmellado@gmail.com>
2022-10-08 08:14:59 -0300

    creacion de modulo compartido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ''// localhost:4200/
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
